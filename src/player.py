183ea75e305ee447314c09c4bf7545ef851ddabe
Alexander Halpern <halperna22@gmail.com>
2024-03-23 16:50:25 -0400

    player updates

--- src/player.py
+++ src/player.py
@@ -51,8 +51,8 @@ class Player:
             'assets/OldStandardTT-Bold.ttf', 10)
         self.fps = 60
         self.timer = pygame.time.Clock()
-        self.WIDTH = 36 * 25  # mess with this
-        self.HEIGHT = 400
+        self.WIDTH = 52 * 25  # mess with this
+        self.HEIGHT = 300
         self.screen = pygame.display.set_mode([self.WIDTH, self.HEIGHT])
         self.white_sounds = []
         self.black_sounds = []
@@ -123,36 +123,42 @@ class Player:
         pygame.quit()
 
     def draw_piano(self, whites, blacks):
+        white_key_width = 60  # New width for white keys
+        black_key_width = 34  # New width for black keys
+
         white_rects = []
         for i in range(36):
             rect = pygame.draw.rect(
-                self.screen, 'white', [i * 35, self.HEIGHT - 300, 35, 300], 0, 2)
+                self.screen, 'black', [i * white_key_width, self.HEIGHT - 300, white_key_width, 300], 0, 2)
             white_rects.append(rect)
             pygame.draw.rect(self.screen, 'black', [
-                i * 35, self.HEIGHT - 300, 35, 300], 2, 2)
+                i * white_key_width, self.HEIGHT - 300, white_key_width, 300], 2, 2)
             key_label = self.small_font.render(
                 self.white_notes[i], True, 'black')
-            self.screen.blit(key_label, (i * 35 + 3, self.HEIGHT - 20))
+            self.screen.blit(
+                key_label, (i * white_key_width + 3, self.HEIGHT - 20))
+
         skip_count = 0
         last_skip = 2
         skip_track = 2
         black_rects = []
         for i in range(25):
+            x_position = 23 + (i * white_key_width) + \
+                (skip_count * white_key_width)
             rect = pygame.draw.rect(self.screen, 'black', [
-                                    23 + (i * 35) + (skip_count * 35), self.HEIGHT - 300, 24, 200], 0, 2)
+                                    x_position, self.HEIGHT - 300, black_key_width, 200], 0, 2)
             for q in range(len(blacks)):
                 if blacks[q][0] == i:
                     if blacks[q][1] > 0:
                         pygame.draw.rect(self.screen, 'green', [
-                            23 + (i * 35) + (skip_count * 35), self.HEIGHT - 300, 24, 200], 0, 2)
+                                         x_position, self.HEIGHT - 300, black_key_width, 200], 0, 2)
                         blacks[q][1] -= 1
 
             key_label = self.real_small_font.render(
                 self.black_labels[i], True, 'white')
-            self.screen.blit(key_label, (25 + (i * 35) +
-                                         (skip_count * 35), self.HEIGHT - 120))
+            self.screen.blit(key_label, (x_position + 1, self.HEIGHT - 120))
             black_rects.append(rect)
-            skip_track += 1
+
             if last_skip == 2 and skip_track == 3:
                 last_skip = 3
                 skip_track = 0
@@ -166,7 +172,7 @@ class Player:
             if whites[i][1] > 0:
                 j = whites[i][0]
                 pygame.draw.rect(self.screen, 'green', [
-                    j * 35, self.HEIGHT - 100, 35, 100], 0, 2)
+                    j * white_key_width, self.HEIGHT - 100, white_key_width, 100], 0, 2)
                 whites[i][1] -= 1
 
         return white_rects, black_rects, whites, blacks
